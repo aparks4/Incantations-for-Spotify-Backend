2e04fd5ca56f97ac3a6659000f9c12d24ff6c2c1
aparks4 <arynmparks@gmail.com>
2022-09-16 13:58:38 -0700

    started working on a test route for userauth thru spotify

--- server.js
+++ server.js
@@ -1,16 +1,34 @@
 const express = require('express');
 
-
 const app = express();
+const querystring = require('querystring');
 
 require("dotenv").config();
 
-const {PORT} = process.env;
+const {PORT, CLIENT_ID, CLIENT_SECRET} = process.env;
+const client_id = CLIENT_ID;
+const client_secret = CLIENT_SECRET;
+const redirect_uri = 'http://localhost:4000';
+
 
 // test route
 app.get('/', (req, res) => {
     res.send('hello world');
 })
+
+app.get('/login', (req, res) => {
+    const state = generateRandomString(16);
+    const scope = 'user-read-private user-read-email';
+    res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: client_id,
+      scope: scope,
+      redirect_uri: redirect_uri,
+      state: state
+    }));
+});
+
 
 
 // SERVER
