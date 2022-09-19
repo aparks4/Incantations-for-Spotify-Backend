d0d474c09254491af98c6d8e25829c61259a7884
aparks4 <arynmparks@gmail.com>
2022-09-19 16:28:55 -0700

    UserAuth code running with no errors, returning a response

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const SpotifyWebApi = require('spotify-web-api-node');
 const app = express();
 app.use(cors())
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 
 require("dotenv").config();
@@ -14,19 +15,8 @@ require("dotenv").config();
 const {PORT, CLIENT_ID, CLIENT_SECRET} = process.env;
 const client_id = CLIENT_ID;
 const client_secret = CLIENT_SECRET;
-const redirect_uri = 'http://localhost:4000';
+const redirect_uri = 'http://localhost:3000';
 
-
-spotifyApi.refreshAccessToken().then(
-  (data) => {
-    console.log(data)
-
-    // Save the access token so that it's used in future calls
-    spotifyApi.setAccessToken(data.body['access_token']);
-  }).catch(() => {
-    res.sendStatus(400)
-  })
-})
 
 // test login with user auth
 
@@ -37,8 +27,23 @@ app.post('/login', (req, res) => {
     clientId: client_id,
     clientSecret: client_secret,
   })
+
+    spotifyApi
+    .authorizationCodeGrant(code)
+    .then(data => {
+        res.json({
+            accessToken: data.body.access_token,
+            refreshToken: data.body.refresh_token,
+            expiresIn: data.body.expires_in,
+        })
+    })
+    .catch(err => {
+        res.sendStatus(400)
+    })
+})
+
   
-  app.post('/refresh', (req, res) => {
+app.post('/refresh', (req, res) => {
     const refreshToken = req.body.refreshToken
     const spotifyApi = new SpotifyWebApi({
       redirectUri: redirect_uri,
@@ -46,21 +51,19 @@ app.post('/login', (req, res) => {
       clientSecret: client_secret,
       refreshToken
     })
-  })
 
-  spotifyApi
-  .authorizationCodeGrant(code)
-  .then(data => {
-    res.json({
-      accessToken: data.body.access_token,
-      refreshToken: data.body.refresh_token,
-      expiresIn: data.body.expires_in,
+    spotifyApi
+    .refreshAccessToken()
+    .then(data => {
+      res.json({
+        accessToken: data.body.access_token,
+        expiresIn: data.body.expires_in,
+      })
+    })
+    .catch(() => {
+      res.sendStatus(400)
     })
   })
-  .catch(() => {
-    res.sendStatus(400)
-  })
- })
 
 
 
