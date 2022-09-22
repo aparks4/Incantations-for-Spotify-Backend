9a9fb7456546c42604ed0fc1c2dff0397449ba31
“philberdecio” <philberdecio@gmail.com>
2022-09-22 18:04:34 -0500

    add plylist routes

--- controllers/playlists-controller.js
+++ controllers/playlists-controller.js
@@ -3,14 +3,33 @@ const express = require('express');
 const router = express.Router();
 
 // MODELS
-const { Playlists } = require('../models');
+const { Playlist } = require('../models');
 
 // ROUTES
 
-// playlist show route
+// PLAYLIST INDEX ROUTE
 router.get('/', async (req, res) => {
     res.send('hello');
 })
+
+// PLAYLIST CREATE ROUTE
+router.post('/', async (req, res) => {
+    try {
+        res.json(await Playlist.create(req.body));
+    } catch (error) { 
+        res.status(400).json(error);
+    }
+});
+
+// PLAYLIST SHOW ROUTE
+router.get("/:id", async (req, res) => {
+    try {
+      res.json(await Playlist.findById(req.params.id));
+    } catch (error) {
+      res.status(400).json(error);
+    }
+  });
+        
 
 
 module.exports = router;
